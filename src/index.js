29a58a42aac7556e0853f520a7d0f085c6751ac2
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-06-21 14:30:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    activeTodo(e.target.create-task-form.value);
+    form.reset()})
 });
+
+function activeTodo(todo){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  p.textContent = todo
+  btn.textContent = "x"
+  btn.addEventListener("click", deleteTodo)
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+}
+function deleteTodo(e){
+  e.target.parentNode.remove()
+}
